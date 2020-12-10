d98fcb9b1f4b17ea3cc586992482a16092a46125
javs1 <59986245+javs1@users.noreply.github.com>
2020-12-09 23:46:16 -0600

    agregando informacion al backend

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import info from './info';
 
 const app= express();
-app.get("/api/productos"), (req,res)=>{
+app.get("/api/productos", (req,res)=>{
     res.send(info.productos);
 
-}
+});
 app.listen(5000, ()=>{console.log("Inicio de servidor http://localhost:5000")})
